d182c25d0579420d3f637d5d9301711779c21568
KimYiuLui <kimyiu-lui@hotmail.com>
2020-12-12 15:59:30 +0100

    styling and adding overview page

--- TheBookWasBetterFlask/TheBookWasBetterFlask/TheBookWasBetterFlask/models/descriptionSimilarity.py
+++ TheBookWasBetterFlask/TheBookWasBetterFlask/TheBookWasBetterFlask/models/descriptionSimilarity.py
@@ -1,4 +1,5 @@
 from TheBookWasBetterFlask import db
+from datetime import datetime
 
 class DescriptionSimilarity(db.Model):
     __tablename__ = "DescriptionSimilarities"
@@ -6,12 +7,14 @@ class DescriptionSimilarity(db.Model):
     similar_book = db.Column(db.Integer, primary_key=True)
     cosine = db.Column(db.Float())
     user_feed = db.Column(db.Integer)
+    last_modified = db.Column(db.DateTime())
 
     def __init__(self, book_id, similar_book, cosine, user_feed):
         self.book_id = book_id
         self.similar_book = similar_book
         self.cosine = cosine
         self.user_feed = user_feed
+        self.last_modified = datetime.now()
 
     def __repr__(self):
         return '%s/%s/%s/%s' % (self.book_id, self.similar_book, self.cosine, self.user_feed)
--- TheBookWasBetterFlask/TheBookWasBetterFlask/TheBookWasBetterFlask/views.py
+++ TheBookWasBetterFlask/TheBookWasBetterFlask/TheBookWasBetterFlask/views.py
@@ -10,6 +10,7 @@ from .models.author import Author
 from .models.descriptionSimilarity import DescriptionSimilarity
 from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from  sqlalchemy.sql.expression import func
 from sqlalchemy import text
 from flask_cors import CORS
 import yaml
@@ -23,33 +24,62 @@ import json
 #    db.session.commit()
 
 
-#@app.route('/')
-#@app.route('/home')
-@app.route('/', methods=['GET'], defaults={"page": 1}) 
-@app.route('/<int:page>', methods=['GET'])
-def index(page=1):
-    per_page = 100
-    books = Book.query.order_by(Book.book_id).with_entities(Book.book_id,Book.title, Book.image_url).paginate(page,per_page,error_out=False)
+@app.route('/')
+@app.route('/home')
+def index():
+    newest_books = Book.query.filter_by(publication_year = datetime.now().year)\
+                       .order_by(Book.book_id.desc())\
+                       .with_entities(Book.book_id,Book.title, Book.image_url)\
+                       .limit(5)
+
+    best_rated_books = Book.query.filter(Book.average_rating > 4.8)\
+                                 .order_by(Book.average_rating.desc() , func.random())\
+                                 .with_entities(Book.book_id,Book.title, Book.image_url)\
+                                 .limit(15)
 
     return render_template(
         'index.html',
         title='Home Page',
         year=datetime.now().year,
+        newest_books = newest_books,
+        best_rated_books = best_rated_books
+    ) 
+
+@app.route('/browse', methods=['GET'], defaults={"page": 1}) 
+@app.route('/browse/<int:page>', methods=['GET'])
+def book(page=1):
+    per_page = 100
+    books = Book.query.order_by(Book.book_id)\
+                      .with_entities(Book.book_id,Book.title, Book.image_url)\
+                      .paginate(page,per_page,error_out=False)
+
+    return render_template(
+        'bookoverviewpage.html',
+        title='Book Overview Page',
+        year=datetime.now().year,
         books = books
     )
 
-#@app.route('/book/', methods=['GET'], defaults={book_id: "3"})
 @app.route("/book/<book_id>")
 def bookdetail(book_id):
-    book = Book.query.filter_by(book_id = book_id).first_or_404()
-    simDesc = DescriptionSimilarity.query.filter_by(book_id = book_id).order_by(DescriptionSimilarity.cosine.desc()).with_entities(DescriptionSimilarity.similar_book).limit(20)
+    book = Book.query.filter_by(book_id = book_id).first()
+    sim_desc = DescriptionSimilarity.query.filter_by(book_id = book_id)\
+                                    .order_by(DescriptionSimilarity.cosine.desc())\
+                                    .with_entities(DescriptionSimilarity.similar_book)\
+                                    .all()
+    sim_desc = [r for r, in sim_desc] 
 
+    sim_books = Book.query.filter(Book.book_id.in_(sim_desc))\
+                          .with_entities(Book.book_id,Book.title, Book.image_url)\
+                          .all()
+
+    print(sim_books)
     return render_template(
         'bookpage.html',
-        title='Home Page',
+        title='Book Detail Page',
         year=datetime.now().year,
         book = book,
-        simDesc = simDesc
+        sim_books = sim_books,
     )
 
 #@app.route('/contact')
